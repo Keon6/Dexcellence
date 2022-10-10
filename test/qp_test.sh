d41f9e03205df18207618826d8a74346bfb4fa52
Kevin Kim <kevinkim1207@gmail.com>
2022-10-09 20:32:53 -0400

    more files

--- test/qp_test.sh
+++ test/qp_test.sh
@@ -6,7 +6,7 @@ do
   for i in 100 500 1000 5000
   do
     echo Number of Assets = "$n" "|" Number of Orders = "$i"
-    nohup python3 qp_speed_test.py -n $n -i $i > qp_speed_test_N="$n"_I="$i".log 2>&1 &
+    nohup python3 qp_speed_test.py -n $n -i $i > logs/qp_speed_test_N="$n"_I="$i".log 2>&1 &
     echo $!
     wait $!
   done
